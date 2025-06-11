41acf782df126cf78454d8d0b0f08b790fdf721c
ghDecapy <josh.mci112@gmail.com>
2025-06-11 16:36:10 +0100

    Baby's first code with extra function

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 def main():
     book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
-    print(book_text)
+    book_whole_text = get_book_text(book_path)
+    num_words = get_book_num(book_whole_text)
+    print(f"{num_words} words found in the document")
 
 def get_book_text(path):
     with open(path) as p:
         return p.read()
+    
+def get_book_num(book_text):
+    book_words_list = book_text.split()
+    return len(book_words_list)
     
 
 main()
